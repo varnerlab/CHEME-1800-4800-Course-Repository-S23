3e6d3721cf677867b273c7f6b837615325a60592
Jeffrey Varner <jdv27@cornell.edu>
2023-01-31 15:10:19 -0500

    Update lab-2b-scope.jl
    messing around w/variable names

--- labs/lab-2b/lab-2b-scope.jl
+++ labs/lab-2b/lab-2b-scope.jl
@@ -8,17 +8,19 @@ Prints the value of the argument that is passed in.
 
 println defined in Base: https://docs.julialang.org/en/v1/base/io-network/#Base.println
 """
-function test_function(x::Float64)
-    
+function test_function(y::Float64)::Nothing
+
     # change the value of x -
-    # x = 2*x;
+    y = 2*y;
+
     
     # what value was passed in -
-    println("What is the value of x = $(x) inside test_function")
+    println("What is the value of y = $(y) inside test_function")
 end
+
 
 # What is global value of x?
 println("What is the global value of x = $(x)")
 
 # call test function -
-y = test_function(64.6)
+z = test_function(x)
