ba413dd5fd6caf47b8e40c66f72c89e247105545
Jeffrey Varner <jdv27@cornell.edu>
2023-02-06 06:05:36 -0500

    moving file
    mv file

--- labs/lab-3b/src/Lab-3b-Sorting-Lib.jl
+++ labs/lab-3b/src/Lab-3b-Sorting-Lib.jl
@@ -5,8 +5,8 @@ Sorts an array of integers into ascending order.
 
 See: https://en.wikipedia.org/wiki/Bubble_sort
 """
-function bubble_sort(arr::Array{Int64, 1})::Array{Int64,1}
-    
+function bubble_sort(arr::Array{Int64,1})::Array{Int64,1}
+
     # initialize -
     N = length(arr)
 
@@ -14,7 +14,7 @@ function bubble_sort(arr::Array{Int64, 1})::Array{Int64,1}
     for i ∈ 1:N
         for j ∈ 1:N-i
             if arr[j] > arr[j+1]
-                
+
                 # swap that values at j and j+1
                 tmp = arr[j]
                 arr[j] = a[j+1]
@@ -38,8 +38,8 @@ Sorts an array of Float64 values into ascending order.
 
 See: https://en.wikipedia.org/wiki/Bubble_sort
 """
-function bubble_sort(arr::Array{Float64, 1})::Array{Float64,1}
-    
+function bubble_sort(arr::Array{Float64,1})::Array{Float64,1}
+
     # initialize -
     N = length(arr)
 
@@ -47,7 +47,7 @@ function bubble_sort(arr::Array{Float64, 1})::Array{Float64,1}
     for i ∈ 1:N
         for j ∈ 1:N-i
             if arr[j] > arr[j+1]
-                
+
                 # swap that values at j and j+1
                 tmp = arr[j]
                 arr[j] = arr[j+1]
@@ -60,5 +60,26 @@ function bubble_sort(arr::Array{Float64, 1})::Array{Float64,1}
     end
 
     # return sorted array -
+    return arr
+end
+
+"""
+    insertion_sort(arr::Array{Int64, 1})
+
+Sorts an array of Float64 values into ascending order.
+
+See: https://en.wikipedia.org/wiki/Insertion_sort
+"""
+function insertion_sort(arr::Array{Float64,1})::Array{Float64,1}
+    n = length(arr)
+    for i in 2:n
+        key = arr[i]
+        j = i - 1
+        while j >= 1 && arr[j] > key
+            arr[j+1] = arr[j]
+            j -= 1
+        end
+        arr[j+1] = key
+    end
     return arr
 end
