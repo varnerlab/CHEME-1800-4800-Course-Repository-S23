f57e31f740a88c8d678260207de8e562330d2c4c
Jeffrey Varner <jdv27@cornell.edu>
2023-02-06 06:13:37 -0500

    Update fibonacci.jl
    update

--- examples/fibonacci/fibonacci.jl
+++ examples/fibonacci/fibonacci.jl
@@ -1,3 +1,11 @@
+"""
+    fibonacci_while_loop(n::Int64) -> Dict{Int64, Int64}
+
+Computes the `fibonacci` sequence for 0 to n where n >= 1. 
+This function returns a dictionary holding the Fibonacci sequence
+
+Fibonacci sequence: https://en.wikipedia.org/wiki/Fibonacci_number
+"""
 function fibonacci_while_loop(n::Int64)::Dict{Int64, Int64}
 
     # check: is n legit?
@@ -32,3 +40,34 @@ function fibonacci_while_loop(n::Int64)::Dict{Int64, Int64}
     # return dictionary -
     return fibonacci_seq;
 end
+
+
+"""
+    fibonacci_for_loop(n::Int64) -> Array{Int64,1}
+
+Computes the `fibonacci` sequence for 0 to n where n >= 1. 
+This function returns an array holding the Fibonacci sequence
+
+See: https://en.wikipedia.org/wiki/Fibonacci_number
+"""
+function fibonacci_for_loop(n::Int64)::Array{Int64,1}
+
+
+    # check: is n legit? n>=1
+    # ...
+
+    # initialize -
+    sequence = zeros(n+1); # why n+1 (and not n)?
+
+    # we know the first two elements -
+    sequence[1] = 0;
+    sequence[2] = 1;
+
+    # main loop -
+    for i in 3:(n+1)
+        sequence[i] = sequence[i-1] + sequence[i-2]
+    end
+
+    # return -
+    return sequence
+end 
