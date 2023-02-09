83527eb409f4e76465772bdbab7a6f5ffac71a73
Jeffrey Varner <jdv27@cornell.edu>
2023-02-09 15:14:54 -0500

    Update Lab3d-Encryption-Lib.jl
    encrypt and decrypt impls

--- labs/lab-3d/MyDNACryptographyPackage/src/Lab3d-Encryption-Lib.jl
+++ labs/lab-3d/MyDNACryptographyPackage/src/Lab3d-Encryption-Lib.jl
@@ -3,9 +3,25 @@
 """
 function encrypt(plaintext::String)::Dict{Int64,String}
     
-    # implement me -
-    # ...
-    
+    # initialize -
+    message = Dict{Int64,String}()
+    counter = 0;
+
+    # build encryptionkey -
+    encryption_model = _build(DNAEncryptionKey);
+    encryptionkey = encryption_model.encryptionkey;
+
+    for c ∈ uppercase(plaintext)
+
+        # encrypt -
+        message[counter] = encryptionkey[c]
+
+        # update the counter -
+        counter = counter + 1
+    end
+
+    # return -
+    return message
 end
 
 """
@@ -13,7 +29,27 @@ end
 """
 function decrypt(encrypteddata::Dict{Int64,String})::String
 
-    # implement me -
-    # ...
+   # initialize -
+   number_of_chars = length(encrypteddata)
+   inverse_encryptionkey_dict = Dict{String, Char}()
+   plaintext = Vector{Char}()
 
+   # build encryptionkey -
+   encryption_model = _build(DNAEncryptionKey);
+   encryptionkey = encryption_model.encryptionkey;
+
+   # build the inverse_key -
+   for (key, value) ∈ encryptionkey
+       inverse_encryptionkey_dict[value] = key
+   end
+
+   for i ∈ 0:(number_of_chars - 1)
+       
+       codon = encrypteddata[i]
+       value = inverse_encryptionkey_dict[codon]
+       push!(plaintext, value)
+   end
+
+   # return -
+   return String(plaintext)
 end
