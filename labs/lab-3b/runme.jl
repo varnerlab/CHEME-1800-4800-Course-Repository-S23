2d4bbf35f2c44072ae363ab27a6a8410e5cd0b3e
Jeffrey Varner <jdv27@cornell.edu>
2023-02-14 13:24:17 -0500

    data
    data

--- labs/lab-3b/runme.jl
+++ labs/lab-3b/runme.jl
@@ -2,7 +2,7 @@
 include("Include.jl")
 
 # setup -
-N = 512
+N = 2048
 test_run = @benchmarkable bubble_sort(rand(N));
 tune!(test_run)
 result = run(test_run)
--- labs/lab-4b/runme.jl
+++ labs/lab-4b/runme.jl
@@ -2,7 +2,7 @@
 include("Include.jl")
 
 # setup -
-N = 512
+N = 2048
 test_run = @benchmarkable quicksort(rand(N));
 tune!(test_run)
 result = run(test_run)
