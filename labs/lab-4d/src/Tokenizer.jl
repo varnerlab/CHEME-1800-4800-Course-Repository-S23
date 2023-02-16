a7644c6c8477c005804c306b012c18a67577d2c4
Jeffrey Varner <jdv27@cornell.edu>
2023-02-16 13:23:57 -0500

    Update Tokenizer.jl
    updated comments

--- labs/lab-4d/src/Tokenizer.jl
+++ labs/lab-4d/src/Tokenizer.jl
@@ -1,3 +1,8 @@
+"""
+    _tokenize(arr::Array{Char,1}, stack::Array{Char,1}, tokens::Array{String,1})
+
+Encodes recursive descent approach to tokenize a string (assuming space as token)
+"""
 function _tokenize(arr::Array{Char,1}, stack::Array{Char,1}, tokens::Array{String,1})
 
     # initialize 
@@ -15,7 +20,6 @@ function _tokenize(arr::Array{Char,1}, stack::Array{Char,1}, tokens::Array{Strin
         # If a space, then we hit a space, join the words in the stack, and add to the tokens array. Clear the stack
         # Finally, descend another level
 
-
         # pop a char from arr -
         next_char = pop!(arr)
         if (isspace(next_char) == false && in(next_char, set_of_chars_to_skip) == false)
@@ -25,13 +29,14 @@ function _tokenize(arr::Array{Char,1}, stack::Array{Char,1}, tokens::Array{Strin
 
         else
             
-            # if we hit a space, then package up the stack, and add to the tokens array -
+            # we hit a space!
+            # When we hit a space, package up the stack (using the join function), and add the resulting string to the tokens array -
             token = join(stack)
             if (isempty(token) == false)
                 push!(tokens, token)
             end
 
-            # clean out the stack -
+            # clean out the stack (erase what has been stored, so we can store new characters)
             empty!(stack);
         end
 
