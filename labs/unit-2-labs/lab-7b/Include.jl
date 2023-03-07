76e5bcfc8651f9d54af24e5df015ce5721960163
Jeffrey Varner <jdv27@cornell.edu>
2023-03-07 05:47:57 -0500

    Skeleton
    codes

--- labs/unit-2-labs/lab-7b/Include.jl
+++ labs/unit-2-labs/lab-7b/Include.jl
@@ -7,4 +7,6 @@ const _PATH_TO_DATA = joinpath(_ROOT, "data")
 using LinearAlgebra
 
 # load my codes -
-# ...
+include(joinpath(_PATH_TO_SRC, "Types.jl"))
+include(joinpath(_PATH_TO_SRC, "Files.jl"))
+include(joinpath(_PATH_TO_SRC, "Compute.jl"))
