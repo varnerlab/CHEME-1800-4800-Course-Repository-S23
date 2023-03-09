430be90bb36d3b997841f6fe933e89d69c42b616
Jeffrey Varner <jdv27@cornell.edu>
2023-03-09 10:56:12 -0500

    Working on Lab 7d
    lab 7d

---
+++ labs/unit-2-labs/lab-7d/runme.jl
@@ -0,0 +1,26 @@
+# include -
+include("Include.jl")
+
+# setup test system -
+# system matrix
+A = [
+    3.0 -0.3 -0.2   ; 
+    0.1 7.0 -0.3    ;
+    0.3 -0.2 10.0   ;
+];
+
+# right hand side vector
+b = [
+    7.85    ;
+    -19.3   ;
+    71.4    ;
+];
+
+# run JacobiIterationSolver -
+# x_jacobi = solve(JacobiIterationSolver(), A, b, zeros(3));
+
+# run the GaussSeidelIterationSolver -
+# x_gs = solve(GaussSeidelIterationSolver(), A, b, zeros(3));
+
+
+
--- labs/unit-2-labs/lab-7d/src/Solvers.jl
+++ labs/unit-2-labs/lab-7d/src/Solvers.jl
@@ -42,13 +42,8 @@ end
 function _gauss_seidel_iteration_solver(A::Array{Float64,2}, b::Array{Float64,1}, xₒ::Array{Float64,1},
     tolerance::Float64, maxiter::Int64)::Array{Float64,1}
 
-    # initialize
-    (number_of_rows, number_of_cols) = size(A);
-    x̂ = xₒ # set the current best answer to the initial guess that was passed
-    should_we_stop = false
-
     # return -
-    return x̂;
+    return gauss_seidel(A,b; maxiter = maxiter)
 end
 
 
