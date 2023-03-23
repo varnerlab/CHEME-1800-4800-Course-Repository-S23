7d2451c9714ddcdb2c53af4d829fa04ac6fb28b2
Jeffrey Varner <jdv27@cornell.edu>
2023-03-23 12:17:33 -0400

    Update Compute.jl
    Lab 9d

--- labs/unit-3-labs/lab-9d/src/Compute.jl
+++ labs/unit-3-labs/lab-9d/src/Compute.jl
@@ -11,21 +11,9 @@ function R(data::DataFrame; r::Float64 = 0.045)::Array{Float64,1}
     # initialize -
     number_of_trading_days = nrow(data);
     r̄ = (1+r)^(1/365) - 1; # convert the annual risk free rate to daily value
-    log_excess_return_array = Array{Float64,1}(undef,  number_of_trading_days - 1)
     
-    # main loop -
-    for i ∈ 2:number_of_trading_days
-        
-        # grab yesterday's close price
-        P₁ = data[i-1,:close];
-        P₂ = data[i, :close];
-
-        # compute the excess return -
-        log_excess_return_array[i-1] = log(P₂/P₁) - r̄
-    end
-
-    # return -
-    return log_excess_return_array;
+    # default -
+    return zeros(10);
 end
 
 
