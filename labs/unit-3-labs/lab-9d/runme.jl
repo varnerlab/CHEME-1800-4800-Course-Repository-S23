f94ced2fc6128e0d5803ece8e0b3530171a14c94
Jeffrey Varner <jdv27@cornell.edu>
2023-03-24 03:50:56 -0400

    change
    removed space

--- labs/unit-3-labs/lab-9d/runme.jl
+++ labs/unit-3-labs/lab-9d/runme.jl
@@ -6,7 +6,22 @@ D_AMD = loaddataset(joinpath(_PATH_TO_DATA, "AMD-OHLC-daily-T03-22-23-F03-22-21.
 D_SPY = loaddataset(joinpath(_PATH_TO_DATA, "SPY-OHLC-daily-T03-22-23-F03-22-21.csv"));
 
 # Step 1: estimate α, β
-# Ok, what do we do now?
+RAMD = R(D_AMD);
+RSPY = R(D_SPY);
+
+# build the data matrix -
+number_of_returns = length(RAMD);
+X = Array{Float64,2}(undef,number_of_returns,2);
+y = Array{Float64,1}(undef,number_of_returns)
+for i ∈ 1:number_of_returns
+    X[i,1] = 1.0
+    X[i,2] = RSPY[i]
+    y[i] = RAMD[i]
+end
+
+# compute parameters -
+β̂ = pinv(X)*y;
 
 # Step 2: estimate the error model.
-# Hmmm. What do I need to do?
+errors = y - X*β̂; 
+ϵ = errormodel(errors);
--- labs/unit-3-labs/lab-9d/src/Compute.jl
+++ labs/unit-3-labs/lab-9d/src/Compute.jl
@@ -13,10 +13,21 @@ function R(data::DataFrame; r::Float64 = 0.045)::Array{Float64,1}
     r̄ = (1+r)^(1/365) - 1; # convert the annual risk free rate to daily value
 
     # TODO: compute the excess returns, store them in an array.
-    # ...
+    log_excess_return_array = Array{Float64,1}(undef,  number_of_trading_days - 1)
     
-    # default return: you'll need to change this to your array
-    return zeros(10);
+    # main loop -
+    for i ∈ 2:number_of_trading_days
+        
+        # grab yesterday's close price
+        P₁ = data[i-1, :close]; # yesterday
+        P₂ = data[i, :close];   # today
+
+        # compute the excess return -
+        log_excess_return_array[i-1] = log(P₂/P₁) - r̄
+    end
+
+    # return -
+    return log_excess_return_array;
 end
 
 
@@ -35,7 +46,8 @@ function errormodel(residuals::Array{Float64,1})::Normal
     σ = 1.0; # default value for the standard deviation
 
     # TODO: compute the standard deviation of the residuals
-    # ...
+    μ = mean(residuals);
+    σ = std(residuals);
 
     # return N(μ,σ)
     return Normal(μ,σ);
