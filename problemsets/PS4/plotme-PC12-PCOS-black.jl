788652c185f353f80c6b8a829baa4985d005b307
Jeffrey Varner <jdv27@cornell.edu>
2023-03-27 19:44:26 -0400

    Nice figures
    nice figures

---
+++ problemsets/PS4/plotme-PC12-PCOS-black.jl
@@ -0,0 +1,70 @@
+# inlclude the Submit.jl script 
+include("Submit.jl");
+
+# add some plot specifc packages
+using Plots
+using Colors
+
+# load the data -
+path_to_data_file = joinpath(_PATH_TO_DATA,"Synthetic-PCOS-Numerical-CoV-Stablized-1000-Fibrinolysis.csv")
+(LPCOS,XPCOS,list_of_measurements) = load_patient_data_file(path_to_data_file);
+
+# setup colors -
+colors = Dict{Int,RGB}();
+colors[1] = colorant"#0077BB"
+colors[2] = colorant"#BBBBBB"
+colors[3] = colorant"#EE7733"
+colors[4] = colorant"#88CCEE"
+
+# make a scatter plot for each point -
+U = F.U; # get the U matrix -
+W = U[:,1:2]
+Y = X*W;
+YPCOS = XPCOS*W
+number_of_patients = size(Y,1);
+for i ∈ 1:number_of_patients
+
+    # which visit is this?
+    visit_index = L[i]
+
+    # get data and colors -
+    x = Y[i,1];
+    y = Y[i,2];
+    c = colors[visit_index]
+
+    # what do we want to plot?
+    if (visit_index == 1 || visit_index == 3)
+        scatter!([x],[y],c=c,label="", ylim=[-10.1,10.1], xlim=[-10.1,10.1], foreground_color_axis=:white, 
+            foreground_color_grid="white", framestyle = :box, bg = :black,
+            foreground_color_border = :white, legend_font_color = :white,
+            fg_legend = :transparent, foreground_color=:black, foreground_color_text=:white,
+            yguidefontcolor=:white,xguidefontcolor=:white, xlabel="Principal component 1", ylabel="Principal component 2", fontsize=18);
+    end
+end
+current();
+
+number_of_patients_PCOS = size(YPCOS,1);
+for i ∈ 1:number_of_patients_PCOS
+
+    # which visit is this?
+    visit_index = LPCOS[i]
+
+    # get data and colors -
+    x = YPCOS[i,1];
+    y = YPCOS[i,2];
+    c = colors[4]
+
+    # what do we want to plot?
+    if (visit_index == 1 || visit_index == 3)
+        scatter!([x],[y],c=c,label="", ylim=[-10.1,10.1], xlim=[-10.1,10.1], foreground_color_axis=:white, 
+        foreground_color_grid="white", framestyle = :box, bg = :black,
+        foreground_color_border = :white, legend_font_color = :white,
+        fg_legend = :transparent, foreground_color=:black, foreground_color_text=:white);
+    end
+end
+
+current();
+# xlabel!("Principle component 1", fontsize=18)
+# ylabel!("Principle component 2", fontsize=18)
+
+
---
+++ problemsets/PS4/plotme-Ucoeff-black.jl
@@ -0,0 +1,231 @@
+# inlclude the Submit.jl script 
+include("Submit.jl");
+
+# packages-
+using PyPlot
+using PyCall
+using Colors
+@pyimport matplotlib.patches as patches
+
+# define my colors -
+negative_color = (1/255)*[187,187,187]
+positive_color = (1/255)*[204,51,17]
+# negative_color = colorant"#BBBBBB"
+# positive_color = colorant"#EE7733"
+
+
+function calculate_mean_centered_array(data_array)
+
+    # what is the dimension of the data array?
+    (number_of_rows, number_of_cols) = size(data_array)
+
+    # initialize -
+    mean_centered_array = zeros(number_of_rows,1)
+
+    # mean center each col -
+    for col_index = 1:number_of_cols
+
+        # grab a col from the sensitvity array -
+        col_data = data_array[:,col_index]
+
+        # mean center -
+        μ = mean(col_data)
+        centered_col = col_data .- μ
+
+        # package -
+        mean_centered_array = [mean_centered_array centered_col]
+    end
+
+    # cut off the leading col of zeros -
+    mean_centered_array = mean_centered_array[:,2:end]
+end
+
+function calculated_scaled_data_array(data_array,a,b)
+
+    # what is the size?
+    (number_of_rows, number_of_cols) = size(data_array)
+
+    # initialize -
+    scaled_data_array = zeros(number_of_rows, number_of_cols)
+    for col_index = 1:number_of_cols
+
+        # find the min and max -
+        min_value = minimum(data_array[:,col_index])
+        max_value = maximum(data_array[:,col_index])
+
+
+        for row_index = 1:number_of_rows
+
+            old_value = data_array[row_index, col_index]
+
+            if (min_value == 0.0 && max_value == 0.0)
+                scaled_data_array[row_index, col_index] = 0.0
+            else
+                new_value = a + ((old_value - min_value)*(b - a))/(max_value - min_value)
+                scaled_data_array[row_index, col_index] = new_value
+            end
+        end
+    end
+
+
+    # return -
+    return scaled_data_array
+end
+
+function calculate_x_axis_tick_positions(data_array;box_width=1.0)
+
+    # what is the size?
+    (number_of_rows, number_of_cols) = size(data_array)
+
+    # initialize -
+    x_axis_points = Float64[]
+    epsilon = 0.50;
+
+    for col_index = 1:number_of_cols
+
+        # compute origin point -
+        origin_point = (col_index - 1)+(col_index - 1)*epsilon + 1
+        push!(x_axis_points,origin_point+0.5*box_width)
+    end
+
+    return x_axis_points
+end
+
+function calculate_y_axis_tick_positions(data_array; box_height=1.0)
+
+    # what is the size?
+    (number_of_rows, number_of_cols) = size(data_array)
+
+    # initialize -
+    y_axis_points = Float64[]
+
+    for col_index = 1:number_of_cols
+
+        # get the data array -
+        data_scaled = data_array[:,col_index]
+
+        # how many patches per col?
+        number_of_patches = length(data_scaled)
+        epsilon = 0.5;
+        for row_index = 1:number_of_patches
+
+            # compute origin point -
+            origin_point = [(col_index - 1)+(col_index - 1)*epsilon + 1,(row_index - 1)+(row_index - 1)*epsilon+1];
+
+            push!(y_axis_points,origin_point[2]+0.5*box_height)
+        end
+    end
+
+    return y_axis_points
+end
+
+function visualize(data_array,x_axis_ticks,y_axis_ticks,y_label_array,a,b)
+
+    # what is the size?
+    (number_of_rows, number_of_cols) = size(data_array)
+
+    epsilon = 0.5;
+    fig=plt.figure()
+    plt.style.use("dark_background")
+    ax = fig.gca()
+
+    # set the axis tick postions -
+    ax.set_xticks(x_axis_ticks)
+    ax.set_yticks(y_axis_ticks)
+    ax.set_yticklabels(y_label_array)
+    subplots_adjust(left=0.25)
+
+    # create x-label array -
+    x_label_array = ["PC$(x)" for x=1:number_of_cols]
+    ax.set_xticklabels(x_label_array, rotation=90.0)
+    ax.spines["right"].set_visible(false)
+    ax.spines["top"].set_visible(false)
+    ax.spines["bottom"].set_visible(false)
+    ax.spines["left"].set_visible(false)
+
+    for col_index = 1:number_of_cols
+
+        # get the data array -
+        data_scaled = data_array[:,col_index]
+
+        # how many patches per col?
+        number_of_patches = length(data_scaled)
+        for row_index = 1:number_of_patches
+
+            # compute origin point -
+            origin_point = [(col_index - 1)+(col_index - 1)*epsilon + 1,(row_index - 1)+(row_index - 1)*epsilon+1];
+
+            # compute alpha -
+            data_value = data_array[row_index, col_index]
+            if (data_value == 0.0)
+
+                # compute a new color -
+                patch_face_color = (0.5)*(negative_color .+ positive_color)
+
+                # draw the square -
+                ax.add_patch(
+
+                           patches.Rectangle(origin_point,   # (x,y)
+                               1.0,          # width
+                               1.0,          # height
+                               facecolor=patch_face_color,
+                               edgecolor="white",
+                               linewidth=0.5,
+                           )
+                       )
+
+            else
+
+                beta = (data_value - a)/(b - a)
+
+                # compute a new color -
+                patch_face_color = (1-beta)*negative_color .+ beta*positive_color
+
+                # draw the square -
+                ax.add_patch(
+
+                           patches.Rectangle(origin_point,   # (x,y)
+                               1.0,          # width
+                               1.0,          # height
+                               facecolor=patch_face_color,
+                               edgecolor="white",
+                               linewidth=0.5,
+                               alpha = 1 - ((col_index-1)/number_of_cols)^0.99
+                           )
+                       )
+            end
+        end
+    end
+
+
+    axis("square")
+end
+
+# load the sensitvity data -
+data_array = abs.(F.U[:,1:10]);
+
+# read my case labels -
+case_labels_array = list_of_measurements;
+
+# calculate the tick locations -
+x_axis_points = calculate_x_axis_tick_positions(data_array)
+y_axis_points = calculate_y_axis_tick_positions(data_array)
+
+labels = [
+    case_labels_array ; # 1
+    case_labels_array ; # 2
+    case_labels_array ; # 3
+    case_labels_array ; # 4
+    case_labels_array ; # 5
+
+    case_labels_array ; # 6
+    case_labels_array ; # 7
+    case_labels_array ; # 8
+    case_labels_array ; # 9
+    case_labels_array ; # 10
+]
+
+# mean center -
+mean_centered_array = calculate_mean_centered_array(data_array)
+scaled_data_array = calculated_scaled_data_array(mean_centered_array, 0, 1)
+visualize(scaled_data_array, x_axis_points, y_axis_points, labels, 0, 1)
