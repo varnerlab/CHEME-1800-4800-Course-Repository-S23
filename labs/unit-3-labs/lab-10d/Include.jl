c64a6d4a33fd337d7e75fa07ae472ffdd3a9719f
Jeffrey Varner <jdv27@cornell.edu>
2023-03-29 16:30:39 -0400

    working on Lab 10d
    lab 10d

--- labs/unit-3-labs/lab-10d/Include.jl
+++ labs/unit-3-labs/lab-10d/Include.jl
@@ -6,3 +6,8 @@ const _PATH_TO_DATA = joinpath(_ROOT, "data")
 # load reqd packages -
 using JuMP
 using GLPK
+
+# load my codes -
+include(joinpath(_PATH_TO_SRC, "Types.jl"))
+include(joinpath(_PATH_TO_SRC, "Files.jl"))
+include(joinpath(_PATH_TO_SRC, "Factory.jl"))
