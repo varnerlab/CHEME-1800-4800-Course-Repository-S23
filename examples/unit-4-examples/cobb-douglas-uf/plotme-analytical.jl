8b3fd835639767b192365f39af24a611e1bd4b2a
Jeffrey Varner <jdv27@cornell.edu>
2023-04-15 02:14:22 -0400

    changes
    changes

--- examples/unit-4-examples/cobb-douglas-uf/plotme-analytical.jl
+++ examples/unit-4-examples/cobb-douglas-uf/plotme-analytical.jl
@@ -8,14 +8,22 @@ colors[2] = colorant"#88CCEE"
 colors[3] = colorant"#EE7733"
 colors[4] = colorant"#CC3311"
 
-# mess around -
-α₁ = 0.5
+# initialize -
+α₁ = 0.5 
 α₂ = 1.0 - α₁
+
+# storage: holds indiffernce curves 
+results = Dict{Int64,Array{Float64,2}}()
+
+# set values for the good and service 1
 X1 = range(0.001,stop=100.0,step = 0.001) |> collect;
 d = length(X1);
-U = [12.0,24.0,36.0,48.0];
+
+# set utility values
+U = [12.0, 24.0, 36.0, 48.0];
 n = length(U);
 
+# simulation loop
 for i ∈ 1:n
     
     Y = Array{Float64,2}(undef,d,2);
@@ -28,6 +36,9 @@ for i ∈ 1:n
         Y[j,1] = X1[j];
         Y[j,2] = exp(tmp);
     end
+
+    # store -
+    results[i] = Y;
 
     plot!(Y[:,1],Y[:,2],xlim=[0,100.0],ylim=[0,100.0],c=colors[i], lw=3, label="U: $(U_val)", 
         bg="floralwhite", background_color_outside="white", framestyle = :box, fg_legend = :transparent)
