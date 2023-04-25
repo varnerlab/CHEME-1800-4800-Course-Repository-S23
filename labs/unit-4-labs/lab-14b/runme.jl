227327aa4877ac58596f155952d475ef97823e94
Jeffrey Varner <jdv27@cornell.edu>
2023-04-25 14:38:28 -0400

    Update runme.jl
    lab 14 b soln

--- labs/unit-4-labs/lab-14b/runme.jl
+++ labs/unit-4-labs/lab-14b/runme.jl
@@ -2,7 +2,7 @@
 include("Include.jl")
 
 # initialize -
-number_of_sample_paths = 1000; # number of instance of the game
+number_of_sample_paths = 10000; # number of instance of the game
 results = Dict{Int, Stack{MyCoinFlipGameModel}}() # this data structure holds a stack of coin flip results for each instance of the game
 p = 0.5; # fair coin, so = 0.5
 d = Bernoulli(p); # to get a sample from this distribution, call rand(d)
@@ -17,21 +17,35 @@ for i ∈ 1:number_of_sample_paths
     s = Stack{MyCoinFlipGameModel}()
 
     # TODO: Implement the coin flip game logic here
-    # set flag
-    # set count = 1
-    # while (flag == true)
-        # flip = rand(d)
-        # if (flip == 1)
-            # we hit a heads
-            # current_payout = current_payout + 2^{count}
+    # setup a flag - and while loop
+    can_flip_again = true
+    flip_index = 1;
+    while (can_flip_again == true)
 
+        # flip -
+        coin_flip_result = rand(d);
+        if (coin_flip_result == 1) # heads
+            
+            # update the current payout -
+            current_payout += 2^(flip_index)
 
-        # else
-            # we hit a tails
+            # we are going around again, update the flip index
+            flip_index = flip_index + 1
+        else
 
-        # end
-    
-    # end
+            # ok, ww hit a tails, stop the game -
+            can_flip_again = false;
+        end
+
+        # build the result of this flip -
+        flip_model = build(MyCoinFlipGameModel, (
+            payout = current_payout,
+            result = coin_flip_result
+        ));
+
+        # push this onto the stack
+        push!(s, flip_model)
+    end
 
 
     # store the stack for this instance of the game
