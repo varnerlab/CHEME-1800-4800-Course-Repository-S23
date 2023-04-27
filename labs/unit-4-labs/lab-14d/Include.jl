2264ff5f85adc04330277b5ef887cad3b8a3c2e8
Jeffrey Varner <jdv27@cornell.edu>
2023-04-27 11:41:32 -0400

    blank testme
    blank test

--- labs/unit-4-labs/lab-14d/Include.jl
+++ labs/unit-4-labs/lab-14d/Include.jl
@@ -10,6 +10,9 @@ using JLD2
 using FileIO
 using Dates
 
+# load some *optional* packages
+using PrettyTables
+
 # load my codes -
 include(joinpath(_PATH_TO_SRC, "Types.jl"))
 include(joinpath(_PATH_TO_SRC, "Factory.jl"))
--- labs/unit-4-labs/lab-14d/testme.jl
+++ labs/unit-4-labs/lab-14d/testme.jl
@@ -1,4 +1,12 @@
 # include the include -
 include("Include.jl")
 
-# 
+# set parameters that are *constant* for all contracts -
+B = 365.0       # Days in a year (all values are per year)
+DTE = 51.0      # Days to expiration (DTE``)
+μ = 0.0352      # risk free rate: https://www.cnbc.com/quotes/US10Y
+L = 100;        # number of levels on the tree
+ticker = "AMD"  # let's look at AMD contracts
+
+# TODO impl a test simulation and compare results with midpoint values
+# ...
