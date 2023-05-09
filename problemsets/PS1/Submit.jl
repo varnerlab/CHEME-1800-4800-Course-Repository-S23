13796e86583978bb9d100419bc82f47c5515532a
Jeffrey Varner <jdv27@cornell.edu>
2023-05-09 13:53:01 -0400

    Missing PS1 soln
    missing PS1 soln

--- problemsets/PS1/Submit.jl
+++ problemsets/PS1/Submit.jl
@@ -3,3 +3,14 @@ include("Include.jl")
 
 # text the teaching team will check -
 plaintext = "Julia is awesome. The computer is my best friend, and it loves me. This is a test and only a test."
+
+# create encryption -
+encryptionmodel = build(DNAEncryptionKey);
+# model = DNAEncryptionKey
+
+# encrypted string -
+encrypted_string = encrypt(plaintext, encryptionmodel = encryptionmodel);
+
+# decrypt -
+decrypted_string = decrypt(encrypted_string, encryptionmodel = encryptionmodel)
+
--- problemsets/PS1/src/CHEME-1800-ProblemSet-1-Lib.jl
+++ problemsets/PS1/src/CHEME-1800-ProblemSet-1-Lib.jl
@@ -3,14 +3,61 @@
 
 Fill me in
 """
-function encrypt(plaintext::String)::Dict{Int64,String}
-end
+function encrypt(plaintext::String; 
+    encryptionmodel::Union{Nothing, AbstractEncryptionKey} = nothing)::Dict{Int64,String}
 
+    # check: do we have a key?
+
+    # initialize -
+    message = Dict{Int64, String}();
+    counter = 0;
+
+    # grab the key -
+    encryptionkey = encryptionmodel.encryptionkey
+
+    # main - iterate through upper case -
+    for c ∈ uppercase(plaintext)
+
+        # encrypt -
+        message[counter] = encryptionkey[c]
+
+        # update the counter -
+        counter = counter + 1
+    end
+
+    # return -
+    return message
+end
 
 """
     decrypt(encrypteddata::Dict{Int64,String}) -> String
 
 Fill me in
 """
-function decrypt(encrypteddata::Dict{Int64,String})::String
+function decrypt(encrytedtext::Dict{Int64,String}; 
+    encryptionmodel::Union{Nothing, AbstractEncryptionKey} = nothing)::String
+
+    # initialize -
+    number_of_chars = length(encrytedtext)
+    inverse_key_dict = Dict{String, Char}()
+    message = Vector{Char}()
+
+    # grab the key -
+    encryptionkey = encryptionmodel.encryptionkey
+
+    # build the inverse_key -
+    for (key, value) ∈ encryptionkey
+        inverse_key_dict[value] = key
+    end
+
+    for i ∈ 0:(number_of_chars - 1)
+        
+        codon = encrytedtext[i]
+        value = inverse_key_dict[codon]
+        push!(message, value)
+    end
+
+    # return -
+    return String(message)
 end
+
